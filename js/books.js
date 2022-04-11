7ef8830632666275b762fb969c96926027f481c5
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-04-11 12:31:42 +0300

    Search and display section for book updated

--- js/books.js
+++ js/books.js
@@ -1,38 +1,61 @@
-const searchButton = document.getElementById("searchButton").value;
-const bookNameInput = document.getElementById("bookInput").value;
-let booksNumber = document.getElementById("bookNumbers").innerHTML;
-const bookContainer = document.getElementById("container").innerHTML;
+const searchButton = document.getElementById("searchButton")
+const bookNameInput = document.getElementById("bookInput")
+const booksNumber = document.getElementById("bookNumbers")
+const bookContainer = document.getElementById("container")
+const errorText = document.getElementById("errorText")
 // Fetch API
-const search = () => {
-    const url = `https://openlibrary.org/search.json?q=${bookNameInput}`
-    fetch(url) 
-    .then(res => res.json()) 
-    .then(data => displayBooks(data.docs));
-}
+searchButton.addEventListener('click', function () {
+    const bookName = bookNameInput.value
+    if (bookName === ''){
+        errorText.innerText = "Please write name of a book!";
+        bookContainer.innerHTML = '';
+        booksNumber.innerHTML = '';
+        return errorText;
+    }
+    else {
+        const url = ` https://openlibrary.org/search.json?q=${bookName}`
+        bookContainer.innerHTML = '';
+        errorText.innerText ='';
+        booksNumber.innerHTML = '';
+        fetch(url) 
+            .then(res => res.json()) 
+            .then(data => displayBooks(data.docs));
+    }
+})
 
 const displayBooks = (docs) => {
     // total book number
     console.log(docs.length)
     const totalBook = docs.length;
     booksNumber.innerText = `Total Search Results : ${totalBook}`
-
+    if (docs.length === 0) {
+        errorText.innerText = 'No book found'
+        bookNameInput.value = ''
+        return errorText
+      } else {
+        errorText.innerText = "";
+      }
 
 // Display book
 docs.forEach(doc => {
 
     const div = document.createElement("div")
     div.classList.add("col-md-3")
-    div.innerHTML = `<div class="col"> 
-    <div class="card h-150 rounded border border-light border-2">
-        <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg"class="card-img-top" height='400'  alt="Book Cover Not Found">
-        <div class="card-body">
-        <h4 class="fw-bold text-danger "> ${doc.title}</h4>
-        <h6 class="text-secondary">Authors: ${doc.author_name}</h6>
-        <h6 class="text-secondary">First Published:   ${doc.first_publish_year}</h6>
-        </div>
-    </div>
+    div.innerHTML = `
+    <div class="col"> 
+                <div class="card h-150 rounded border border-secondary border-2">
+                    <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg"class="card-img-top" height='400'  alt="Book Cover Not Found">
+                    <div class="card-body">
+                    <h4 class="fw-bold text-dark "> ${doc.title}</h4>
+                    <h6 class="text-danger">Authors: ${doc.author_name}</h6>
+                    <h6 class="text-danger">First Published:   ${doc.first_publish_year}</h6>
+                    <h6 class="text-secondary">Language:  ${doc.language}</h6>
+                    <h6 class="text-secondary">Genre:  ${doc.type}</h6>
+                    </div>
+                </div>
     </div>
     `
-    bookContainer.appendhild(div)
+    bookContainer.appendChild(div);
+    bookNameInput.value = '';
 })
 }
